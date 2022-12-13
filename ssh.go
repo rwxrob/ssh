114954ca44c153af27cd6bbb7842aa3755291f84
Rob Muhlestein <rob@rwx.gg>
2022-12-13 01:33:41 -0500

    Add MultiHostClient

--- ssh.go
+++ ssh.go
@@ -2,10 +2,16 @@ package ssh
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 
 	"golang.org/x/crypto/ssh"
 )
+
+// TCPTimeout is the default number of seconds to wait to complete a TCP
+// connection.
+var TCPTimeout = 300 * time.Second
 
 // Run wraps the ssh.Session.Run command with sensible, stand-alone
 // defaults. This function has no dependencies on any underlying ssh
@@ -49,16 +55,17 @@ func Run(target string, ukey, hkey []byte, cmd, in string) (stdout, stderr strin
 		return
 	}
 
-	conn, err := ssh.Dial(`tcp`, addr, &ssh.ClientConfig{
+	client, err := ssh.Dial(`tcp`, addr, &ssh.ClientConfig{
 		User:            user,
 		Auth:            []ssh.AuthMethod{ssh.PublicKeys(signer)},
 		HostKeyCallback: ssh.FixedHostKey(hostpub),
+		Timeout:         TCPTimeout,
 	})
 	if err != nil {
 		return
 	}
 
-	sess, err := conn.NewSession()
+	sess, err := client.NewSession()
 	if err != nil {
 		return
 	}
@@ -78,4 +85,156 @@ func Run(target string, ukey, hkey []byte, cmd, in string) (stdout, stderr strin
 
 	return
 
+}
+
+// ------------------------------- User -------------------------------
+
+// we use an interface for flexibility and to allow the work to create
+// a signer to only be needed once upon creation
+
+type User struct {
+	Name   string
+	Key    []byte // original pemkey
+	Signer ssh.Signer
+}
+
+func NewUser(name string, pemkey []byte) (*User, error) {
+	var err error
+	u := new(User)
+	u.Name = name
+	u.Key = pemkey
+	u.Signer, err = ssh.ParsePrivateKey(pemkey)
+	if err != nil {
+		return u, err
+	}
+	return u, nil
+}
+
+// ------------------------------- Host -------------------------------
+
+// we use an interface for flexibility and to allow the work to create
+// a signer to only be needed once upon creation
+
+type Host struct {
+	Addr    string        // name or IP
+	Auth    []byte        // authorized_hosts format
+	Netkey  ssh.PublicKey // RFC 4235, section 6.6
+	Pubkey  ssh.PublicKey // suitable for ssh.FixedHostkey
+	Comment string        // authorized_hosts comment
+	Options []string      // authorized_hosts options
+}
+
+func NewHost(addr string, authkey []byte) (*Host, error) {
+	var err error
+	host := new(Host)
+	host.Addr = addr
+	host.Auth = authkey
+
+	host.Netkey, host.Comment, host.Options, _, err = ssh.ParseAuthorizedKey(authkey)
+	if err != nil {
+		return host, err
+	}
+
+	// required since host.net (also ssh.PublicKey) is in RFC format
+	// (which fails for ssh.FixedHostKey)
+
+	host.Pubkey, err = ssh.ParsePublicKey(host.Netkey.Marshal())
+	if err != nil {
+		return host, err
+	}
+
+	return host, nil
+}
+
+// -------------------------- MultiHostClient -------------------------
+
+type MultiHostClient struct {
+	User     *User
+	Hosts    []*Host
+	Timeout  time.Duration
+	Attempts int
+
+	last int
+}
+
+func (c MultiHostClient) assert() {
+	switch {
+	case c.User == nil:
+		panic(`undefined User`)
+	case c.User.Name == "":
+		panic(`undefined User.Name`)
+	case c.User.Signer == nil:
+		panic(`undefined User.Signer`)
+	case c.Hosts == nil:
+		panic(`undefined Hosts`)
+	case c.Timeout == 0:
+		panic(`Timeout cannot be 0`)
+	case c.Attempts == 0:
+		panic(`Attempts cannot be 0`)
+	}
+}
+
+// Run attempts to dial a random host from Hosts and waits the Timeout
+// duration for a TCP connection before moving to the next host in Hosts
+// and attempting to Dial it repeating the cycle once until number of
+// Attempts is reached. The total amount of time to wait is therefore
+// equal to Timeout * Attempts. The first host to respond to Dial is used.
+// Note that the err returned by a command does not cause additional
+// attempts, only failed Dail attempts. Panics if any User, Hosts,
+// Timeout, or Attempts is undefined.
+func (c *MultiHostClient) Run(cmd, in string) (stdout, stderr string, err error) {
+	c.assert()
+
+	c.last = rand.Intn(len(c.Hosts))
+	host := c.Hosts[c.last]
+	var client *ssh.Client
+
+	// keep trying until attempt exhausted
+	for attempts := 0; attempts < c.Attempts; attempts++ {
+
+		client, err = ssh.Dial(`tcp`, host.Addr, &ssh.ClientConfig{
+			User:            c.User.Name,
+			Auth:            []ssh.AuthMethod{ssh.PublicKeys(c.User.Signer)},
+			HostKeyCallback: ssh.FixedHostKey(host.Pubkey),
+			Timeout:         c.Timeout,
+		})
+
+		// error during dial
+		if err != nil {
+			if c.last == len(c.Hosts)-1 {
+				c.last = 0
+			} else {
+				c.last++
+			}
+		}
+
+		host = c.Hosts[c.last]
+
+	}
+
+	if client == nil {
+		return
+	}
+
+	// successful dialup
+	var sess *ssh.Session
+	sess, err = client.NewSession()
+	if err != nil {
+		return
+	}
+
+	if in != "" {
+		sess.Stdin = strings.NewReader(in)
+	}
+
+	_out := new(strings.Builder)
+	_err := new(strings.Builder)
+	sess.Stdout = _out
+	sess.Stderr = _err
+
+	err = sess.Run(cmd)
+	stdout = _out.String()
+	stderr = _err.String()
+
+	return
 }
--- ssh_test.go
+++ ssh_test.go
@@ -1,5 +1,13 @@
 package ssh_test
 
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rwxrob/ssh"
+)
+
 /*
 func ExampleRun() {
 	ukey, _ := os.ReadFile(`testdata/blahpriv`)
@@ -12,3 +20,96 @@ func ExampleRun() {
 	// ignored
 }
 */
+
+/*
+func ExampleMultiHostClient_Run() {
+
+	c := new(ssh.MultiHostClient)
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println(p)
+		}
+	}()
+	ukey, _ := os.ReadFile(`testdata/blahpriv`)
+	c.User, _ = ssh.NewUser(`blah`, ukey)
+	hkey, _ := os.ReadFile(`testdata/hostpubkey`)
+	host, _ := ssh.NewHost(`localhost:22`, hkey)
+	dummy, _ := ssh.NewHost(`localhost:23434`, hkey) // will fail
+	c.Hosts = []*ssh.Host{dummy, host}
+	c.Timeout = 10 * time.Second
+	c.Attempts = 1
+
+	stdout, stderr, err := c.Run(`cat`, `hello`)
+	fmt.Printf("STDOUT\n%v\n", stdout)
+	fmt.Printf("STDERR\n%v\n", stderr)
+	fmt.Printf("ERROR\n%v\n", err)
+
+	// Output:
+	// ignored
+}
+*/
+
+func ExampleMultiHostClient_run_assert_user() {
+	c := new(ssh.MultiHostClient)
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println(p)
+		}
+	}()
+	c.Run(`ls -l ~`, "")
+
+	// Output:
+	// undefined User
+}
+
+func ExampleMultiHostClient_run_assert_hosts() {
+	c := new(ssh.MultiHostClient)
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println(p)
+		}
+	}()
+	key, _ := os.ReadFile(`testdata/blahpriv`)
+	c.User, _ = ssh.NewUser(`blah`, key)
+	c.Run(`ls -l ~`, "")
+	// Output:
+	// undefined Hosts
+}
+
+func ExampleMultiHostClient_run_assert_timeout() {
+	c := new(ssh.MultiHostClient)
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println(p)
+		}
+	}()
+	ukey, _ := os.ReadFile(`testdata/blahpriv`)
+	c.User, _ = ssh.NewUser(`blah`, ukey)
+	hkey, _ := os.ReadFile(`testdata/hostpubkey`)
+	host, _ := ssh.NewHost(`localhost`, hkey)
+	c.Hosts = []*ssh.Host{host}
+
+	c.Run(`ls -l ~`, "")
+	// Output:
+	// Timeout cannot be 0
+}
+
+func ExampleMultiHostClient_run_assert_attempts() {
+
+	c := new(ssh.MultiHostClient)
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println(p)
+		}
+	}()
+	ukey, _ := os.ReadFile(`testdata/blahpriv`)
+	c.User, _ = ssh.NewUser(`blah`, ukey)
+	hkey, _ := os.ReadFile(`testdata/hostpubkey`)
+	host, _ := ssh.NewHost(`localhost`, hkey)
+	c.Hosts = []*ssh.Host{host}
+	c.Timeout = 10 * time.Second
+
+	c.Run(`ls -l ~`, "")
+	// Output:
+	// Attempts cannot be 0
+}
